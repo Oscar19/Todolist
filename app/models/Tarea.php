62faef247d74ae9f6390670c962c8d2003357392
Oscar <oscanguera@gmail.com>
2024-07-04 10:58:22 +0200

    Proyecto acabado

--- app/models/Tarea.php
+++ app/models/Tarea.php
@@ -1,48 +1,60 @@
-<?php 
-    class Tarea extends Model{
-        public $id;
-        public $titulo;
-        public $descripcion;
-        public $usuario;
-        public $status;
-        public $fecha;
-        public $horaInicio;
-        public $horaFin;
+<?php
+class Tarea extends Model {
+    protected $tareaJson = "/opt/lampp/htdocs/Todolist/config/Tareas.json";
+    protected array $tareas;
 
-        private $tareaJson;
-
-        public function __construct($tareaJson = null)
-        {
-           
-            $this->tareaJson = "/opt/lampp/htdocs/Todolist/config/Tareas.json";
-            if (!file_exists($this->tareaJson)){
-               
-                file_put_contents($this->tareaJson, json_encode([]));
-            }
-
+    public function __construct() {
+        if (!file_exists($this->tareaJson)){
+            file_put_contents($this->tareaJson, json_encode([]));
         }
-       public function getTareas(){
-            // accedemos a todas las tareas
-            return json_decode(file_get_contents($this->tareaJson), true);
-        }
-     
-
-        public function addTarea($id, $titulo, $descripcion, $usuario, $status, $fecha, $horaInicio, $horaFin){
-            $tareas = $this->getTareas();
-            $newId = count($tareas) > 0 ? max(array_column($tareas, 'Id')) + 1 : 1;
-
-            $newTarea = [
-                "Id" => $id,
-                "Titulo" => $titulo,
-                "Descripcion" => $descripcion,
-                "Usuario" => $usuario,
-                "Status" => $status,
-                "Fecha" => $fecha,
-                "Hora_inicio" => $horaInicio,
-                "Hora_final" => $horaFin,
-            ];
-            $tareas[] = $newTarea;
-            file_put_contents($this->tareaJson, json_encode($tareas));
-        }
+        $this->tareas = json_decode(file_get_contents($this->tareaJson), true);
     }
 
+    public function getTareas() {
+        return $this->tareas;
+    }
+
+    public function addTarea($id, $titulo, $descripcion, $usuario, $status, $fecha, $horaInicio, $horaFin) {
+        $newTarea = [
+            "Id" => $id,
+            "Titulo" => $titulo,
+            "Descripcion" => $descripcion,
+            "Usuario" => $usuario,
+            "Status" => $status,
+            "Fecha" => $fecha,
+            "Hora_inicio" => $horaInicio,
+            "Hora_final" => $horaFin,
+        ];
+        $this->tareas[] = $newTarea;
+        file_put_contents($this->tareaJson, json_encode($this->tareas));
+    }
+    
+    //foreach ($this->tareas as &$tarea) 
+    // manera más rápida de llegar al id del objeto que estoy editando
+   public function updateTarea($id, $titulo, $descripcion, $usuario, $status, $fecha, $horaInicio, $horaFin) {
+        foreach ($this->tareas as &$tarea) {
+            if ($tarea['Id'] == $id) {
+                $tarea['Titulo'] = $titulo;
+                $tarea['Descripcion'] = $descripcion;
+                $tarea['Usuario'] = $usuario;
+                $tarea['Status'] = $status;
+                $tarea['Fecha'] = $fecha;
+                $tarea['Hora_inicio'] = $horaInicio;
+                $tarea['Hora_final'] = $horaFin;
+                file_put_contents($this->tareaJson, json_encode($this->tareas));
+                return true; 
+            }
+        }
+        return false; 
+    }
+
+     public function deleteTarea($id)
+    {
+        $tareas = $this->getTareas();
+        //otra manera de llegar rápido al id del objeto
+        $tareas = array_filter($tareas, function($tarea) use ($id) {
+            return $tarea['Id'] != $id;
+        });
+        file_put_contents($this->tareaJson, json_encode(array_values($tareas)));
+    }
+}
